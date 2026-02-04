0919236c0a2f1defafd784ffa88a3edbc1ca030e
Daniel Alves <daniel.alvesmoreira1@gmail.com>
2026-02-04 15:03:01 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnsinoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
