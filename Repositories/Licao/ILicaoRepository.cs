3e241eead950882950c2c4de3416289037589532
Daniel Alves <daniel.alvesmoreira1@gmail.com>
2026-02-11 17:27:03 -0300

    feat: Implementando o repository de Lição

---
+++ Repositories/Licao/ILicaoRepository.cs
@@ -0,0 +1,11 @@
+namespace EnsinoApp.Repositories.Licao;
+
+public interface ILicaoRepository
+{
+
+    Task<IEnumerable<Models.Entities.Licao>> FindByCursoAsync(int cursoId);
+    Task<Models.Entities.Licao> FindByIdAsync(int id);
+    Task<Models.Entities.Licao> CreateAsync(Models.Entities.Licao licao);
+    Task<Models.Entities.Licao> UpdateAsync(Models.Entities.Licao licao);
+    Task DeleteAsync(int id);
+}
---
+++ Repositories/Licao/LicaoRepository.cs
@@ -0,0 +1,58 @@
+
+using EnsinoApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnsinoApp.Repositories.Licao;
+
+
+public class LicaoRepository : ILicaoRepository
+{
+
+    private readonly EnsinoAppContext _context;
+
+    public LicaoRepository(EnsinoAppContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Models.Entities.Licao> CreateAsync(Models.Entities.Licao licao)
+    {
+        _context.Licoes.Add(licao);
+        await _context.SaveChangesAsync();
+        return licao;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var licao = await FindByIdAsync(id);
+        if (licao == null) return;
+
+        // Evitar exclusão se tiver relatório vinculado
+        if (_context.Relatorios.Any(r => r.IdLicao == id))
+            throw new InvalidOperationException("Não é possível excluir lição com relatórios vinculados.");
+
+        _context.Licoes.Remove(licao);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Models.Entities.Licao>> FindByCursoAsync(int cursoId)
+    {
+        return await _context.Licoes
+                .Where(l => l.IdCurso == cursoId)
+                .OrderBy(l => l.NumeroSemana)
+                .ToListAsync();
+
+    }
+
+    public async Task<Models.Entities.Licao> FindByIdAsync(int id)
+    {
+        return await _context.Licoes.FirstOrDefaultAsync(l => l.Id == id);
+    }
+
+    public async Task<Models.Entities.Licao> UpdateAsync(Models.Entities.Licao licao)
+    {
+        _context.Licoes.Update(licao);
+        await _context.SaveChangesAsync();
+        return licao;
+    }
+}
